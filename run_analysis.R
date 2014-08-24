1bf07b7bd1f2b0581236a987f68b19153ad12c50
Wally Thornton <wfthornton@gmail.com>
2014-08-24 09:01:28 -0500

    Additional comments, fixed subject bug

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,28 @@
+####################################################################################
+
+## Coursera Getting and Cleaning Data Course Project
+## Wally Thornton
+## August 24, 2014
+
 # The run_analysis function reads, cleans and reformats activity tracking
 # data collected and made available by the University of California at
-# Irvine
+# Irvine. See README.md file for an overview and Codebook.md for details
+# on reading and running run_analysis.
+
+####################################################################################
 
 run_analysis <- function() {
+    # Set working directory
+    setwd("/users/wallythornton/datasciencecoursera/Getting_and_Cleaning_Data/")
+    
     # Read everything in
     x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
     y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+    subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
     x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
     y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+    x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+    subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
     activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
     features <- read.table("./UCI HAR Dataset/features.txt")
     
@@ -29,9 +44,15 @@ run_analysis <- function() {
     colnames(total_y)[1] <- "Activity"
     final_set <- cbind(total_y, filtered_data)
     
+    # Combine subject groups, rename and add to final_set
+    total_subjects <- rbind(subject_test, subject_train)
+    colnames(total_subjects)[1] <- "Test_Subject"
+    final_set <- cbind(total_subjects, final_set)
+    
     # Melt final_set and recast
-    set_melt <- melt(final_set, id = "Activity", measure.vars = colnames(final_set[,-1]))
-    set_cast <- dcast(set_melt, Activity ~ variable,mean)
+    library(reshape2)
+    set_melt <- melt(final_set, id = c("Test_Subject", "Activity"), measure.vars = colnames(final_set[,-1:-2]))
+    set_cast <- dcast(set_melt, Test_Subject + Activity ~ variable, mean)
     
     # Write out the recasted data set
     write.table(set_cast, file = "./set_cast.txt", row.names=FALSE)
